0bb305c45dbccb7ec677abc23416a4782f4c3b79
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-12-26 18:36:52 +0100

    day 1 part 2

--- d1.go
+++ d1.go
@@ -1,23 +1,48 @@
 package aoc21
 
 import (
-  "strconv"
-  "strings"
+	"strconv"
+	"strings"
 )
 
 func D1p1(input string) int {
-  els := strings.Split(input, "\n")
-  ddepth := 0
-  prev := 0
-  for i, el := range els {
-    el = strings.TrimSpace(el)
-    elint, _ := strconv.Atoi(el)
-    if i > 0 {
-      if prev < elint {
-        ddepth++
-      }
-    }
-    prev = elint
-  }
-  return ddepth
+	els := strings.Split(input, "\n")
+	ddepth := 0
+	prev := 0
+	for i, el := range els {
+		el = strings.TrimSpace(el)
+		elint, _ := strconv.Atoi(el)
+		if i > 0 {
+			if prev < elint {
+				ddepth++
+			}
+		}
+		prev = elint
+	}
+	return ddepth
+}
+
+func D1p2(input string) int {
+	els := strings.Split(input, "\n")
+	ddepth := 0
+	for i, _ := range els {
+		if i > 2 {
+			window1 := sum(els[i-3 : i])
+			window2 := sum(els[i-2 : i+1])
+			if window1 < window2 {
+				ddepth++
+			}
+		}
+	}
+	return ddepth
+}
+
+func sum(els []string) int {
+	sum := 0
+	for _, el := range els {
+		el = strings.TrimSpace(el)
+		elint, _ := strconv.Atoi(el)
+		sum += elint
+	}
+	return sum
 }
--- d1_test.go
+++ d1_test.go
@@ -24,7 +24,7 @@ func TestD1p1(t *testing.T) {
 	for _, c := range cases {
 		got := D1p1(c.in)
 		if got != c.want {
-			t.Errorf("solution of (%q) == %q, want %q", c.in, got, c.want)
+			t.Errorf("solution of (%q) == %d, want %d", c.in, got, c.want)
 		}
 	}
 }
@@ -33,4 +33,34 @@ func TestD1p1solution(t *testing.T) {
 	file := "D1"
 	in := Read(file)
 	fmt.Printf("%s solution: %d\n", file, D1p1(in))
+}
+
+func TestD1p2(t *testing.T) {
+	cases := []struct {
+		in   string
+		want int
+	}{
+		{`199
+200
+208
+210
+200
+207
+240
+269
+260
+263`, 5},
+	}
+	for _, c := range cases {
+		got := D1p2(c.in)
+		if got != c.want {
+			t.Errorf("solution of (%q) == %d, want %d", c.in, got, c.want)
+		}
+	}
+}
+
+func TestD1p2solution(t *testing.T) {
+	file := "D1"
+	in := Read(file)
+	fmt.Printf("%s solution: %d\n", file, D1p2(in))
 }
