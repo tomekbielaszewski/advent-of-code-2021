ec27786100aa84bd430ac520001f82f736e5dff5
Tomasz Bielaszewski <tomekbielaszewski@gmail.com>
2021-12-26 19:39:49 +0100

    day 3 skeleton

---
+++ d3.go
@@ -0,0 +1,9 @@
+package aoc21
+
+func D3p1(input string) int {
+	return 0
+}
+
+func D3p2(input string) int {
+	return 0
+}
--- d3_test.go
+++ d3_test.go
@@ -9,23 +9,24 @@ func TestD3(t *testing.T) {
 		in   string
 		want int
 	}{
-		{`forward 5
-down 5
-forward 8
-up 3
-down 8
-forward 2`, 150},
+		{`00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`, 198},
 	}
 	casesP2 := []struct {
 		in   string
 		want int
 	}{
-		{`forward 5
-down 5
-forward 8
-up 3
-down 8
-forward 2`, 900},
+		{``, 0},
 	}
 	part1 := D3p1
 	part2 := D3p2
